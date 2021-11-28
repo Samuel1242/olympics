283fb3029bc0884791aad7e1f5f4065e94335307
Samuel1242 <94230313+Samuel1242@users.noreply.github.com>
2021-11-28 13:54:55 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+canvas= document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+color="blue"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=4;
+ctx.arc(200, 190, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+canvas= document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+color="orange"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=4;
+ctx.arc(245, 225, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+canvas= document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+color="black"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=4;
+ctx.arc(290, 190, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+canvas= document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+color="green"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=4;
+ctx.arc(335, 225, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+canvas= document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+color="red"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=4;
+ctx.arc(380, 190, 40, 0, 2 * Math.PI);
+ctx.stroke();
